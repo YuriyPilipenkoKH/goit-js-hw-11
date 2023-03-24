5c2ed2287eb95d0c3a87124291169c93f3efd872
Yuriy Pilipenko <yurik2061@gmail.com>
2023-03-24 06:01:49 +0200

    Update fetch.js

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 export { fetchImages };
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '34491623-5c4ef369d6c59f62bc483440c';
+const BASE_URL = 'https://pixabay.com/api';
 
 async function fetchImages(query, page, perPage) {
   try {
     const response = await axios.get(
-  `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+  `${BASE_URL}/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
 );
 return response;
   } catch (error) {
